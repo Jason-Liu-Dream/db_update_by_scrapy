e309a3c753cd7e146d10651e71be7fd717cab772
jason <winrar6521313@163.com>
2016-05-30 11:05:19 +0800

    add settings

---
+++ settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+settings.py
+
+Author: Jason liu
+Date: 2016-04-25
+"""
+
+# MySQL module settings
+HOST = 'localhost'
+PORT = 3306
+USER= 'root'
+PASSWORD = 'test'
+DBNAME = 'test'
